7c437435e4ab1c9795f1e7abc2b1004545c7b1cd
ataulmohsin9000@gmail.com <ataulmohsin900@gmail.com>
2025-09-10 00:17:57 +0500

    update

---
+++ lib/addition.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  stdout.write('Enter your first value:');
+  int num1 = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter your second value:');
+  int num2 = int.parse(stdin.readLineSync()!);
+  int sum = num1 + num2;
+  print(sum);
+}
