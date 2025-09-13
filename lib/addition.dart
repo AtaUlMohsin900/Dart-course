661dc4827cf2af048369f136aebcb5d4d746736d
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-09-13 16:05:00 +0000

    update

--- lib/addition.dart
+++ lib/addition.dart
@@ -1,12 +1,8 @@
-import 'dart:io';
 
-void main() {
-  stdout.write('Enter your first value:');
-  int num1 = int.parse(stdin.readLineSync()!);
-  stdout.write('Enter your second value:');
-  int num2 = int.parse(stdin.readLineSync()!);
-  int sum = num1 + num2;
-  print(sum);
+
+void main(){
+  stdout.write("Inter the first number=");
+  int num1 = int.parse(stdin.readLineSync()!)
 }
 
-
+z
