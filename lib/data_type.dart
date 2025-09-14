ea70894edd4dc5240caaebec04a0b50d34aa8b07
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-09-14 19:19:02 +0000

    update

--- lib/data_type.dart
+++ lib/data_type.dart
@@ -1,7 +1,17 @@
 import 'dart:io';
+// we already read the intiger data type.
+// String
+// void main() {
+//   print('Enter your Name =');
+//   String name = stdin.readLineSync()!;
+//   print('your name is = $name');
+// }
 
-void main() {
-  print('Enter your Name =');
-  String name = stdin.readLineSync()!;
-  print('your name is = $name');
+// double
+
+void main(){
+    print('Enter your flout value =');
+    double  value = double.parse(stdin.readLineSync()!);
+    print(value);
+
 }
