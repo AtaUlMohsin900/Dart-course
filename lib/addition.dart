a8bc785b82734c28a1543fb512cef5902cc8a758
ataulmohsin9000@gmail.com <ataulmohsin900@gmail.com>
2025-09-15 15:43:26 +0500

    update

--- lib/addition.dart
+++ lib/addition.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
-void main(){
+void main() {
   stdout.write('Enter the first value =');
   int num1 = int.parse(stdin.readLineSync()!);
   stdout.write('Enter the second value =');
   int num2 = int.parse(stdin.readLineSync()!);
   stdout.write('Enter the third value = ');
-  int nums3 = int.parse(stdin.readLineSync()!);
-  print(num1 + num2/nums3);
+  int num3 = int.parse(stdin.readLineSync()!);
+  stdout.write('Enter the forth value = ');
+  int num4 = int.parse(stdin.readLineSync()!);
+  print(num1 + num2 - num3 / num4);
 }
