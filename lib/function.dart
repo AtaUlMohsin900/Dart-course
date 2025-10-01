65277a4f289b2cafdc1015fb5c780b7039f97794
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-10-01 05:56:08 +0000

    update

---
+++ lib/function.dart
@@ -0,0 +1,4 @@
+// Functions are the block of code that performs a specific task. They are created when some statements are repeatedly occurring in the program. The function helps reusability of the code in the program.
+// (DRY) Don’t Repeat Yourself is the main objective of the Dart Functions.
+
+
