6dd33a5be22f92169318f184959c0ca0ddff9178
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-10-02 17:25:32 +0000

    example3

--- lib/function.dart
+++ lib/function.dart
@@ -18,3 +18,18 @@ void add(int num1, int num2){
 void main(){
   add(10, 20);
 }
+
+// This function finds simple interest from principal, time and rate and display result.
+
+// function that calculate interest
+void calculateInterest(double principal, double rate, double time) {
+  double interest = principal * rate * time / 100;
+  print("Simple interest is $interest");
+}
+
+void main() {
+  double principal = 5000;
+  double time = 3;
+  double rate = 3;
+  calculateInterest(principal, rate, time);
+}
