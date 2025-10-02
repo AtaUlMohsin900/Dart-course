48b67956647aa1b52e34eb9576cd74110be89632
Ataul Mohsin <103097405+AtaUlMohsin900@users.noreply.github.com>
2025-10-02 17:22:44 +0000

    Example2

--- lib/function.dart
+++ lib/function.dart
@@ -6,3 +6,15 @@
 void printName(){
   print("My name is Raj Sharma. I am from function.");
 }
+
+// This function finds the sum of two numbers. Here, the function accepts two parameters. i.e., num1 and num2, 
+// and the return type is void.
+
+void add(int num1, int num2){
+  int sum = num1 + num2;
+   print("The sum is $sum");
+}
+
+void main(){
+  add(10, 20);
+}
